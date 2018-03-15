a67cc3e9573c4a35354d8ac2a93f1228496da8ba
allenwhitedev <allenwhitedev@gmail.com>
2018-03-15 14:43:47 -0400

    Add mongo, add readme, add .config.js (hidden by gitignore)

---
+++ readme.md
@@ -0,0 +1,1 @@
+Requires a .config.js file with a valid 'mongoUrl' & 'databaseName' exports to successfully connect to a mongo database. 
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const config = require('./.config.js')
 
+// node/express/mongo initialization
 let PORT = 8002
 if ( process.env.PORT )
 	PORT = process.env.PORT
-
 app.listen(PORT, () => console.log(`Node app listening on port ${ PORT }`) )
+
+const mongoUrl = config.mongoUrl 
+const databaseName = config.databaseName 
+
+let globalDatabase = null // database is property of databases returned from MongoClient.connect() if using mongo v>3.4
+MongoClient.connect(mongoUrl, (err, databases) =>
+{
+	if (err) 
+		throw err
+	else
+		globalDatabase = databases.db(databaseName)
+})
+
 
 app.get('/', (req, res) =>
 {
@@ -15,4 +30,14 @@ app.get('/', (req, res) =>
 app.get('/test', (req, res) =>
 {
 	res.send({error: null, message: `Success! from /test route of sampleNodeApp on port ${ PORT }`})
+})
+
+app.get('/tests', (req, res) =>
+{
+	globalDatabase.collection('tests').find().toArray( (err, result) =>
+	{
+		if ( err ) throw err
+
+		res.send(result)
+	} )
 })
